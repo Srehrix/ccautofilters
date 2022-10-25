79f0e68af5ab5071f840b2083f312828e651acf4
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-25 15:20:10 +0530

    Create yt-dlp.py

---
+++ plugins/extra/yt-dlp.py
@@ -0,0 +1,93 @@
+import os
+import asyncio
+import youtube_dl
+
+from pornhub_api import PornhubApi
+from pornhub_api.backends.aiohttp import AioHttpBackend
+from youtube_dl.utils import DownloadError
+
+from pyrogram import Client, filters
+from pyrogram.types import (
+    Message, InlineQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+)
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+async def run_async(func, *args, **kwargs):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, func, *args, **kwargs)
+
+def url(filter, client, update):
+    if "www.pornhub" in update.text:
+        return True
+    else:
+        return False
+
+url_filter = filters.create(url, name="url_filter") 
+
+@Client.on_message(url_filter)
+async def options(c: Client, m: Message):
+    await m.reply_text(
+        "Tap the button to continue action!", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Download", callback_data=f"d_{m.text}",
+                    ),
+                ],[
+                    InlineKeyboardButton(
+                        "watch in web", url=m.text,
+                    ),
+                ],
+            ],
+        ),
+    )
+
+@Client.on_callback_query(filters.regex("^d"))
+async def get_video(c: Client, q: CallbackQuery):
+    url = q.data.split("_",1)[1]
+    msg = await q.message.edit("Downloading...")
+    user_id = q.message.from_user.id
+
+    if "some" in active:
+        await q.message.edit("Sorry, you can only download videos at a time!")
+        return
+    else:
+        active.append(user_id)
+
+    ydl_opts = {
+            "progress_hooks": [lambda d: download_progress_hook(d, q.message, c)]
+        }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            await run_async(ydl.download, [url])
+        except DownloadError:
+            await q.message.edit("Sorry, an error occurred")
+            return
+
+    for file in os.listdir('.'):
+        if file.endswith(".mp4"):
+            await q.message.reply_video(
+                f"{file}",
+                thumb="downloads/src/pornhub.jpeg",
+                width=1280,
+                height=720,
+                caption="The content you requested has been successfully downloaded!",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("• Donate •", url="https://trakteer.id/levina-crqid/tip"),
+                        ],
+                    ],
+                ),
+            )
+            os.remove(f"{file}")
+            break
+        else:
+            continue
