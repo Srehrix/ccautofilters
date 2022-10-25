67f824029495a10a185e2e01cdcace659fc1da5b
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-25 15:30:33 +0530

    Update yt-dlp.py

--- plugins/extra/yt-dlp.py
+++ plugins/extra/yt-dlp.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 import youtube_dl
+import threading
 
 from pornhub_api import PornhubApi
 from pornhub_api.backends.aiohttp import AioHttpBackend
@@ -15,7 +16,7 @@ from pyrogram.types import (
     InlineQueryResultArticle,
     InputTextMessageContent,
 )
-from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden, MessageNotModified, FloodWait
 
 async def run_async(func, *args, **kwargs):
     loop = asyncio.get_running_loop()
@@ -27,7 +28,42 @@ def url(filter, client, update):
     else:
         return False
 
-url_filter = filters.create(url, name="url_filter") 
+url_filter = filters.create(url, name="url_filter")
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+def edit_msg(client, message, to_edit):
+    try:
+        client.loop.create_task(message.edit(to_edit))
+    except FloodWait as e:
+        client.loop.create_task(asyncio.sleep(e.value))
+    except MessageNotModified:
+        pass
+    except TypeError:
+        pass
+
+
+def download_progress_hook(d, message, client):
+    if d['status'] == 'downloading':
+        current = d.get("_downloaded_bytes_str") or humanbytes(int(d.get("downloaded_bytes", 1)))
+        total = d.get("_total_bytes_str") or d.get("_total_bytes_estimate_str")
+        file_name = d.get("filename")
+        eta = d.get('_eta_str', "N/A")
+        percent = d.get("_percent_str", "N/A")
+        speed = d.get("_speed_str", "N/A")
+        to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
+        threading.Thread(target=edit_msg, args=(client, message, to_edit)).start() 
 
 @Client.on_message(url_filter)
 async def options(c: Client, m: Message):
