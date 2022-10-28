73767e1ae842640ad07df3136114bd1a9c22f353
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-28 06:00:59 +0530

    Update yt-dlp.py

--- plugins/extra/yt-dlp.py
+++ plugins/extra/yt-dlp.py
@@ -87,110 +87,21 @@ async def options(c: Client, m: Message):
         ], [
             InlineKeyboardButton('⬇️ 426x240', callback_data=f"d_{m.text}")
         ]]))
-@Client.on_callback_query(filters.regex("^a"))
-async def get_video(c: Client, q: CallbackQuery):
-    url = q.data.split("_",1)[1]
-    msg = await q.message.edit("Downloading...")
-    user_id = q.message.from_user.id
-    
-    ydl_opts = {
-            "progress_hooks": [lambda a: download_progress_hook(a, q.message, c)]
-        }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            await run_async(ydl.download, [url])
-        except DownloadError:
-            await q.message.edit("Sorry, an error occurred")
-            return
-
-    for file in os.listdir('.'):
-        if file.endswith(".mp4"):
-            await q.message.reply_video(
-                f"{file}",
-                width=1920,
-                height=1080,
-                caption="The content you requested has been successfully downloaded!",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("• Donate •", url="https://trakteer.id/levina-crqid/tip"),
-                        ],
-                    ],
-                ),
-            )
-
-@Client.on_callback_query(filters.regex("^b"))
-async def get_video(c: Client, q: CallbackQuery):
-    url = q.data.split("_",1)[1]
-    msg = await q.message.edit("Downloading...")
-    user_id = q.message.from_user.id
-    
-    ydl_opts = {
-            "progress_hooks": [lambda b: download_progress_hook(b, q.message, c)]
-        }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            await run_async(ydl.download, [url])
-        except DownloadError:
-            await q.message.edit("Sorry, an error occurred")
-            return
-
-    for file in os.listdir('.'):
-        if file.endswith(".mp4"):
-            await q.message.reply_video(
-                f"{file}",
-                width=854,
-                height=480,
-                caption="The content you requested has been successfully downloaded!",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("• Donate •", url="https://trakteer.id/levina-crqid/tip"),
-                        ],
-                    ],
-                ),
-            )
-            
-@Client.on_callback_query(filters.regex("^c"))
-async def get_video(c: Client, q: CallbackQuery):
-    url = q.data.split("_",1)[1]
-    msg = await q.message.edit("Downloading...")
-    user_id = q.message.from_user.id
-    
-    ydl_opts = {
-            "progress_hooks": [lambda c: download_progress_hook(c, q.message, c)]
-        }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        try:
-            await run_async(ydl.download, [url])
-        except DownloadError:
-            await q.message.edit("Sorry, an error occurred")
-            return
-
-    for file in os.listdir('.'):
-        if file.endswith(".mp4"):
-            await q.message.reply_video(
-                f"{file}",
-                width=1280,
-                height=720,
-                caption="The content you requested has been successfully downloaded!",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("• Donate •", url="https://trakteer.id/levina-crqid/tip"),
-                        ],
-                    ],
-                ),
-            )
 @Client.on_callback_query(filters.regex("^d"))
 async def get_video(c: Client, q: CallbackQuery):
     url = q.data.split("_",1)[1]
     msg = await q.message.edit("Downloading...")
     user_id = q.message.from_user.id
-    
+    dir = os.listdir()
+        if f"{phid}.jpg" in dir:
+            thumb = f"{phid}.jpg"
+        elif f"{phid}.png" in dir:
+            thumb = f"{phid}.png"
+        elif f"{phid}.webp" in dir:
+            thumb = f"{phid}.webp"
+        else:
+            thumb = None
+
     ydl_opts = {
             "progress_hooks": [lambda d: download_progress_hook(d, q.message, c)]
         }
@@ -206,8 +117,9 @@ async def get_video(c: Client, q: CallbackQuery):
         if file.endswith(".mp4"):
             await q.message.reply_video(
                 f"{file}",
-                width=426,
-                height=240,
+                width=1920,
+                height=1080,
+                thumb=thumb,
                 caption="The content you requested has been successfully downloaded!",
                 reply_markup=InlineKeyboardMarkup(
                     [
