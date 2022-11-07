ea49cdb9b21ae0bec5c12f102b33bc616057a784
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-11-07 19:04:42 +0530

    Update test.py

--- plugins/extra/test.py
+++ plugins/extra/test.py
@@ -21,7 +21,7 @@ from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForb
 active = []
 queues = []
 
-@Client.on_inline_query("p")  & filters.user(ADMINS)
+@Client.on_inline_query(filters.user(ADMINS))
 async def inline_search(c: Client, q: InlineQuery):
     query = q.query
     backend = AioHttpBackend()
