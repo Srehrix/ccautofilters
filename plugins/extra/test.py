d38e502e1755c2f1b16d1d39c846287f269c9092
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-11-08 21:27:05 +0530

    Update test.py

--- plugins/extra/test.py
+++ plugins/extra/test.py
@@ -136,7 +136,19 @@ async def get_video(c: Client, q: CallbackQuery):
     url = q.data.split("_",1)[1]
     msg = await q.message.edit("Downloading...")
     user_id = q.message.from_user.id
- 
+    
+    ydl_opts = {
+            "progress_hooks": [lambda d: download_progress_hook(d, q.message, c)]
+        }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            await run_async(ydl.download, [url])
+        except DownloadError:
+            await q.message.edit("Sorry, an error occurred")
+            return
+
+
     for file in os.listdir('.'):
         if file.endswith(".mp4"):
             await q.message.reply_video(
