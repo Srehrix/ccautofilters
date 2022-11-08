bb3e32d2559d24a220debfd20fe33b09c8db7fa0
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-11-09 05:28:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -50,7 +50,7 @@ Example:- /set_caption 📕 File Name: {filename}
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. {} should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -65,7 +65,7 @@ Example:- /set_caption 📕 File Name: {filename}
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. {} supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
