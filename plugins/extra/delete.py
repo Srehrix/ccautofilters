d810de833521c7c601a018dc385c33a761917b1e
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-11-11 19:46:11 +0530

    Update delete.py

--- plugins/extra/delete.py
+++ plugins/extra/delete.py
@@ -18,7 +18,7 @@ async def db_delete(bot, message):
             InlineKeyboardButton('Cancel', callback_data='close_data')
             ]]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await message.edit_text(
+    await message.reply_text(
         text="**⚠️ Warning !! ❗ Read This Carefully, Otherwise Your Files will lost 🥶**\n\n1. **Select The Size**\n<i>It Will Delete Entire Files Below the Size</i>\n\n2. **This Will Remove all the files containing the below names**\n* <i>Theatre Prints, Website Names Such As Tamilmvu Tamilblasters, HTPMovies, etc.. & Subtitles</i>\n\n3. **This Will Remove all the files like**\n* <i>Images, Documents & Audio</i>",
         reply_markup=reply_markup)
 
