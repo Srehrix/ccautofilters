b561a4e8e7232f774c55cf7aa63bcfbd5123bdb8
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-11-11 19:59:40 +0530

    Update delete.py

--- plugins/extra/delete.py
+++ plugins/extra/delete.py
@@ -22,25 +22,6 @@ async def db_delete(bot, message):
         text="**⚠️ Warning !! ❗ Read This Carefully, Otherwise Your Files will lost 🥶**\n\n1. **Select The Size**\n<i>It Will Delete Entire Files Below the Size</i>\n\n2. **This Will Remove all the files containing the below names**\n* <i>Theatre Prints, Website Names Such As Tamilmvu Tamilblasters, HTPMovies, etc.. & Subtitles</i>\n\n3. **This Will Remove all the files like**\n* <i>Images, Documents & Audio</i>",
         reply_markup=reply_markup)
 
-@Client.on_callback_query(filters.regex('dl_delete'))
-async def db_delete(bot, message):
-    buttons = [[
-            InlineKeyboardButton('Below 10 MB', callback_data='dlta'), 
-            InlineKeyboardButton('Below 30 MB', callback_data='dltb'),
-            InlineKeyboardButton('Below 50 MB', callback_data='dltc')
-            ], [
-            InlineKeyboardButton('File Name', callback_data='dltname'),
-            InlineKeyboardButton('File Type', callback_data='dlttype')
-            ], [
-            InlineKeyboardButton('Delete All Files', callback_data='dltall')
-            ], [
-            InlineKeyboardButton('Cancel', callback_data='close_data')
-            ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply_text(
-        text="**⚠️ Warning !! ❗ Read This Carefully, Otherwise Your Files will lost 🥶**\n\n1. **Select The Size**\n<i>It Will Delete Entire Files Below the Size</i>\n\n2. **This Will Remove all the files containing the below names**\n* <i>Theatre Prints, Website Names Such As Tamilmvu Tamilblasters, HTPMovies, etc.. & Subtitles</i>\n\n3. **This Will Remove all the files like**\n* <i>Images, Documents & Audio</i>",
-        reply_markup=reply_markup)
-
 @Client.on_callback_query(filters.regex('dlta'))
 async def dlta(bot, message):
     buttons = [[
