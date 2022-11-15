9bbfcc15bb51a70704102142183a7329d1187724
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-11-15 06:46:04 +0530

    Delete plugins/extra directory

--- plugins/extra/approve.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters 
-from pyrogram.types import Message, User, ChatJoinRequest
-@Client.on_chat_join_request((filters.group | filters.channel))
-async def autoapprove(bot: Client, message: ChatJoinRequest):
-    chat=message.chat # Chat
-    user=message.from_user # User
-    await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
